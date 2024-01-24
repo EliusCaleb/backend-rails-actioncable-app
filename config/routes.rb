8341e3e882182c20c767b96d2f324d0e8e801660
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2024-01-24 10:15:44 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount ActionCable.server => '/cable'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+ 
+  resources :messages, only: %i[index]
+  resources :users, only: %i[index create] do
+    post 'add_message'
+    post 'change_status'
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
